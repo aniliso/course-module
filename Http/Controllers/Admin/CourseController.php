a871eb6972440db69c38ed6bf72e8ce10fc07e79
aniliso <sagsoz06@gmail.com>
2017-09-24 23:43:58 +0300

    Updating Status Model

--- Http/Controllers/Admin/CourseController.php
+++ Http/Controllers/Admin/CourseController.php
@@ -2,7 +2,7 @@
 
 namespace Modules\Course\Http\Controllers\Admin;
 
-use App\Models\Status;
+use Core\Models\Status;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Modules\Course\Entities\Course;
--- Presenters/CoursePresenter.php
+++ Presenters/CoursePresenter.php
@@ -1,7 +1,7 @@
 <?php
 namespace Modules\Course\Presenters;
 
-use App\Models\Status;
+use Core\Models\Status;
 use Modules\Core\Presenters\BasePresenter;
 
 class CoursePresenter extends BasePresenter
