628180b08054617073530f1cca129ed9c3f5a471
sagsoz06 <sagsoz06@gmail.com>
2017-11-15 03:55:04 +0300

    Commit test

--- Config/config.php
+++ Config/config.php
@@ -6,7 +6,8 @@ return [
     | Array of middleware that will be applied on the page module front end routes
     |--------------------------------------------------------------------------
     */
-    'middleware' => [],
+    'middleware' => [
+    ],
 
     /*
     |--------------------------------------------------------------------------
